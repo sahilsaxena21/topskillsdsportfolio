073814905be91d45c245c8b86ce5d6c176c4406f
Sahil Saxena <48413484+sahilsaxena21@users.noreply.github.com>
2021-02-18 12:16:10 -0500

    Add files via upload
    changed class name, added code comments

--- _01_Scraper.py
+++ _01_Scraper.py
@@ -4,7 +4,11 @@
 # In[12]:
 
 
-'''This script contains two classes. One for crawling job postings, and the other for crawling hard skills'''
+'''
+This script contains two classes.
+1: JobPostingsCollector: for crawling job postings
+2: HardSkillsCollector: for scraping the hard skills
+'''
 
 __author__ = 'Sahil Saxena'
 __email__ = 'sahil.saxena@outlook.com'
@@ -15,28 +19,56 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-class Indeed_Data_Collector:
-    '''This class crawls job postings from indeed.ca, and saves the result in a csv file'''
+class JobPostingsCollector:
+    '''
+    This class crawls and collects job descriptions from job postings. It then saves 
+    the results locally in a csv file.
+    '''
 
     def __init__(self, job_titles_list, job_title_col, url_col, job_description_col,                 label_col, num_pages = 10, existing_file = None):
+        '''       
+        Attributes
+        ----------
+        job_titles_list: list. list of job titles to include as search terms in Indeed.ca
+        job_title_col: str. column name to be used for the job title as found in the job posting
+        url_col: str. column name to be used for the url of the job posting
+        job_description_col: str. column name to be used for the job description as found in the job posting
+        label_col: str. column name to be used for the assigned job group. A job group is assigned from set 
+                     {data scientist, machine learning engineer, data analyst, data engineer} depending 
+                     on the title in the job posting
+        num_pages: int. number of search pages to crawl in search results.
+        existing_file: str. The location of the existing csv file, if one exists. If None, a new csv file will be created
+        indeed_df: df. The final dataframe with the scraped job posting
+        '''
         
-        '''initialize variables'''
         self.job_titles_list = job_titles_list
+        self.job_title_col = job_title_col
+        self.url_col = url_col
+        self.job_description_col = job_description_col
         self.label_col = label_col
         self.num_pages = num_pages
-        self.job_description_col = job_description_col
-        self.url_col = url_col
-        self.job_title_col = job_title_col
         self.indeed_df = self._indeed_crawler(existing_file)
 
     def _load_data(self, filepath):
+        '''Read a csv file from a provided directory. Returns a pandas dataframe'''
         return pd.read_csv(filepath, index_col=[0])
     
     def _write_file(self, df, filepath):
+        '''Write a pandas dataframe to a csv file in the provided directory'''
         df.to_csv(filepath)
     
     def _add_job_group(self, x):
-        '''assign group based on title wording in job posting'''
+        '''
+        Assigns a job group to a job posting based on the wording of the job title.
+        
+        Arguments
+        ----------
+        x: str. the job title string collected from the job posting
+        
+        Returns
+        -------
+        a job group in set {"Data Scientist", "Machine Learning Engineer", "Data Engineer","Data Analyst", "None"} 
+        '''
         
         x = str(x).lower()
         if "data scien" in x:
@@ -51,16 +83,40 @@ class Indeed_Data_Collector:
             return "None"
     
     def _clean_text(self, x):
-        #search for all non-letters and replace with spaces
+        '''
+        Search for all non-letters and replace with spaces
+        
+        Parameters
+        ----------
+        x: str. the text to be cleaned
+        
+        Returns
+        -------
+        letters_only: str. the processed text
+        '''
+        
         letters_only = re.sub("[^a-zA-Z]", " ",str(x))
         return letters_only
         
     def _indeed_crawler(self, existing_file):
-        '''Crawls in a two-step process. First, it gets the job posting URLs from the search results pages. 
-        Then, it visits the urls to get the job descriptions.'''
+        '''
+        Crawls Indeed.ca job postings in a two-step process. 
+        First, it gets the job posting URLs. 
+        Then, it visits each URL to get the job descriptions.
         
-        #perform crawl and update indeed_df
+        Parameters
+        ----------
+        existing_file: the location of the existing csv file, if one exists. If None, a new csv file will be created
+
+        Returns
+        -------
+        a pandas dataframe with job_title, url, job_description and job_group columns
+        '''
+        
+        #step 1: Collect urls and store results in dataframe
         df_urls = self._get_job_links_indeed()
+        
+        #step 2: Get job descriptions by visiting each url. Store results in a new dataframe
         df_new_scrape = self._get_job_descriptions_from_links(df_urls)
         
         #perform basic cleaning of job_description
@@ -82,7 +138,19 @@ class Indeed_Data_Collector:
             return df_updated     
 
     def _get_job_links_indeed(self):
-        '''Crawls a given number of Indeed search pages to obtain the URLs of each posting'''        
+        '''
+        Crawls a given number of search result pages on Indeed.ca 
+        Collects and stores the URLs of job postings.
+        
+        Parameters
+        ----------
+        None
+        
+        Returns
+        ----------
+        a pandas df with job title, job group and url columns
+        
+        '''        
         try:  
             dict_indeed = dict()
             
@@ -113,6 +181,21 @@ class Indeed_Data_Collector:
                 print("Exceeded Daily Ping Limit. Please try again later")
     
     def _get_indeed_description(self, x):
+        '''
+        Parses the job descriptions by visting Indeed.ca webpage URLs.
+        
+        Parameters
+        ----------
+        x: str. the URL of the job posting
+        
+        Returns
+        ----------
+        description: str. the job description text within the job posting
+        
+        Raises
+        ----------
+        error if the ping limit has reached
+        '''
         
         try:  
             source = requests.get(x).text 
@@ -130,8 +213,18 @@ class Indeed_Data_Collector:
     
         
     def _get_job_descriptions_from_links(self, df_urls):
-        '''Takes input df with url column, visits the url and returns df with new column specific info 
-        contained in the url webpage'''
+        '''
+        Gets job descriptions from URLs.
+        
+        Parameters
+        ----------
+        df_urls: pandas df. the dataframe with the urls of the job postings but without the job descriptions
+        
+        Returns
+        ----------
+        df_updated: pandas df. the dataframe with the added job_description_col column that contains the 
+                    job description of each url
+        '''
         df_updated = df_urls.copy()
         df_updated[self.job_description_col] = df_updated[self.url_col].apply(lambda x: self._get_indeed_description(str(x)))
         return df_updated
@@ -151,17 +244,17 @@ if __name__ == '__main__':
     num_pages = 1
     
     job_titles_list = ["data scientist", "machine learning engineer", "data engineer", "data analyst"]
-    data = Indeed_Data_Collector(job_titles_list = job_titles_list, num_pages = num_pages,                                  label_col = label_col, job_description_col = job_description_col,                                 url_col = url_col, job_title_col = job_title_col,                                 existing_file = indeed_csv_file)
+    data = JobPostingsCollector(job_titles_list = job_titles_list, num_pages = num_pages,                                  label_col = label_col, job_description_col = job_description_col,                                 url_col = url_col, job_title_col = job_title_col,                                 existing_file = indeed_csv_file)
     print(data.indeed_df.shape)
 
 
 # In[10]:
 
 
-class Tools_Collector:
-    '''This class crawls hard skills words from analyticsvidhya, datascienceglossary, 
-    Google's ML Glossary, Wiki's ML Glossary and Wiki's list of programming languages.
-    Results are saved in a csv file'''
+class HardSkillsCollector:
+    '''
+    This class crawls hard skills words from various websites and then saves the results locally in a csv file.
+    '''
 
     def __init__(self, word_col, existing_file = None):
         '''initialize variables'''
@@ -169,12 +262,26 @@ class Tools_Collector:
         self.df_word = self._get_words()
     
     def _load_data(self, file):
+        '''Read a csv file from a provided directory. Returns a pandas dataframe'''
         return pd.read_csv(file, index_col=[0])
     
     def _write_file(self, df, file):
+        '''Write a pandas dataframe to a csv file in the provided directory'''        
         df.to_csv(file)
         
     def _clean_text_words(self, x):
+        '''
+        Removes all special characters
+        
+        Parameters
+        ----------
+        x: str. the text to be cleaned
+        
+        Returns
+        -------
+        letters_only: str. the processed text
+        '''
+        
         x = x.lower()
         x = re.sub(r'\([^)]*\)', "", x)
         x = re.sub("_[\_(\_[].*?[\)\]]", "", x)
@@ -182,7 +289,23 @@ class Tools_Collector:
         return x
 
     def _get_words(self):
-        '''crawl various websites to build the hard skills list'''
+        '''
+        Scrapes contents from a number of websites to build the hard skills list. 
+        Websites are:
+        a) AnalyticsVidhya
+        b) DataScienceGlossary.org
+        c) Google Developer's Machine Learning Glossary
+        d) Wikipedia's Glossary of Artifical Intelligence
+        e) Wikipedia's list of programming languages
+        
+        Parameters
+        ----------
+        None
+        
+        Returns
+        -------
+        df_word: pandas df. dataframe of hard skills
+        '''
         words = []
 
         #source analytics vidhya
@@ -239,7 +362,7 @@ if __name__ == '__main__':
 
     #define input files
     word_col = "word"
-    tc = Tools_Collector(word_col)
+    tc = HardSkillsCollector(word_col)
     tc._write_file(tc.df_word, "data/words_final.csv")
     
 
